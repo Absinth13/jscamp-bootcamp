4ed4e19975dfbd35fa32c3abff605c3396f744eb
madeval <alvarez.fing@gmail.com>
2025-12-15 12:39:58 -0300

    refactorización del event listener del botón aplicar para mejorar performance usando delegación de eventos en el contenedor padre

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -1,22 +1,28 @@
 /* Aquí va la lógica para dar funcionalidad al botón de "Aplicar" */
 //-------------------------------------tercera forma----------
-//const jobsListingSection = document.querySelector('.jobs-listings')
+const jobsListingSection = document.querySelector('.jobs-listings')
 
-//jobsListingSection.addEventListener('click', function(event) {
-  //const element = event.target
+jobsListingSection.addEventListener('click', function(event) {
+  const element = event.target
 
-  //if (element.classList.contains('button-apply-job')) {
-  ///  element.textContent = '¡Aplicado!'
-   // element.classList.add('is-applied')
-   // element.disabled = true
-//  }
-//})
+  if (element.classList.contains('button-apply-job')) {
+    element.textContent = '¡Aplicado!'
+    element.classList.add('is-applied')
+    element.disabled = true
+ }
+})
 
-document.addEventListener("click", function (event) {
-  const element = event.target;
-  if (element.classList.contains("button-apply-job")) {
-    element.textContent = "¡Aplicado!";
-    element.classList.add("is-applied");
-    element.disabled = true;
-  }
-});
+/*
+Genial! La manera en la que lo hiciste está excelente.
+Usaría la tercera forma que comentas arriba de este comentario.
+A pesar de que ambas funcionen y sean correctas, la `tercera forma` que planteas reduce la cantidad de veces que se tenga que ejecutar el `addEventListener`
+*/
+
+// document.addEventListener("click", function (event) {
+//   const element = event.target;
+//   if (element.classList.contains("button-apply-job")) {
+//     element.textContent = "¡Aplicado!";
+//     element.classList.add("is-applied");
+//     element.disabled = true;
+//   }
+// });
