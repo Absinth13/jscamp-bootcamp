ae6bdd5675b392bb4e3f5ca8f68b1bb06c9f5221
madeval <alvarez.fing@gmail.com>
2026-04-09 17:51:04 -0300

    Refactor JobListings.jsx: replace ternary with early return for empty jobs array

--- 03-ejercicio-react/src/components/JobListings.jsx
+++ 03-ejercicio-react/src/components/JobListings.jsx
@@ -2,19 +2,24 @@
 
  
   export function JobListings({jobs}){
+    /* Genial! Podemos separar esto en dos `if`, así queda más legible */
+
+    // 1. si no hay resultados, mostramos un mensaje
+    if(jobs.length === 0) {
+      return (
+        <p style={{ textAlign: 'center', width: '100%', padding: '1rem', textWrap:'balance' }}>
+          "No se han encontrado empleos que coincidan con los criterios de búsqueda".🥲
+        </p>
+      )
+    }
+
+    // 2. si hay resultados, los mostramos
     return (
       <>
       <div className="jobs-listings">
-        {jobs.length === 0 ? (
-          <p style={{ textAlign: 'center', width: '100%', padding: '1rem', textWrap:'balance' }}>
-            "No se han encontrado empleos que coincidan con los criterios de búsqueda".🥲
-           </p>
-           
-        ) : (
-          jobs.map(job => (
+        {jobs.map(job => (
             <JobCard key={job.id} job={job} />
-          ))
-        )}
+          ))}
           </div>
         </>
     ) 
